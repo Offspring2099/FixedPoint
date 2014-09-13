519d04c8d2852a27c1a96a4037c8dec66dddc4d8
Offspring2099 <dmitriy@labitov.com>
2014-09-13 14:54:20 -0700

    tabs to spaces

--- fixedpoint.js
+++ fixedpoint.js
@@ -6,28 +6,28 @@
 
 
 /**
- * @param bgWidth 		- width of background image you are using
- * @param bgHeight 		- height of background image you are using
- * @param targetObject 	- target object to be plotted. Ex. "#rock" 
- * @param align 		- 'left' or 'right' - use to top left or top right corner of the targetObject as an anchor
- * @param ptX 			- x coordinate of the object being plotted, relative to the background image
- * @param ptY 			- y coordinate of the object being plotted, relative to the background image
+ * @param bgWidth      - width of background image you are using
+ * @param bgHeight     - height of background image you are using
+ * @param targetObject - target object to be plotted. Ex. "#rock" 
+ * @param align        - 'left' or 'right' - use to top left or top right corner of the targetObject as an anchor
+ * @param ptX          - x coordinate of the object being plotted, relative to the background image
+ * @param ptY          - y coordinate of the object being plotted, relative to the background image
  */
 function FixedPoint(bgWidth, bgHeight, targetObject, align, ptX, ptY) {
-	var self = this;
+    var self = this;
     this.bgHeight = bgHeight;
     this.bgWidth = bgWidth;
     this.targetObject = targetObject;
     this.align = align;
-	this.ptX = ptX;
+    this.ptX = ptX;
     this.ptY = ptY;
 
     this.plot(); 
-	
-	$(window).resize(function() {
-		self.plot();
-	});
-	
+    
+    $(window).resize(function() {
+        self.plot();
+    });
+    
     $(this.targetObject).css("display", "block"); 
 }
 
@@ -35,15 +35,15 @@ function FixedPoint(bgWidth, bgHeight, targetObject, align, ptX, ptY) {
  * Plots the element
  */
 FixedPoint.prototype.plot = function() {
-	var height 	= $(window).height();
-    var width 	= $(window).width();
+    var height     = $(window).height();
+    var width     = $(window).width();
 
     var bgRatio = this.bgWidth / this.bgHeight ;
     var windowRatio = width/height ;
-	
-	var tmp;
-	var newX;
-	var newY;
+    
+    var tmp;
+    var newX;
+    var newY;
 
     //Width is proportionally greater
     if(windowRatio >= bgRatio ) {
@@ -57,12 +57,12 @@ FixedPoint.prototype.plot = function() {
         newX    = Math.floor( ((((this.bgWidth*height)/this.bgHeight)*this.ptX)/this.bgWidth)-tmp );
         newY = Math.floor( (this.ptY*height)/this.bgHeight );
     }
-	
-	if(this.align=="left"){
-		$(this.targetObject).css("left", newX); 
-	}else if(this.align=="right"){
-		$(this.targetObject).css("left", newX-$(this.targetObject).outerWidth());
-	}
-	
-	$(this.targetObject).css("top", newY); 
+    
+    if(this.align=="left"){
+        $(this.targetObject).css("left", newX); 
+    }else if(this.align=="right"){
+        $(this.targetObject).css("left", newX-$(this.targetObject).outerWidth());
+    }
+    
+    $(this.targetObject).css("top", newY); 
 }
